8ef12024cb44bdb3c02a0e57e9db32878a745774
Brandon Forseth <bfclashman15@gmail.com>
2024-02-09 09:14:05 -0700

    oh boy

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+const pages= ["/bacon","/imgflip","/blackjack","/nasa"];
+const numPages = 4; 
+
+
+
+
 
 app.use(express.static("public"));
 
@@ -13,6 +19,37 @@ app.get("/",(req,res) =>{
     res.render("index.ejs");
     
 });
+
+app.get("/about",(req,res) =>{
+    res.render("about.ejs");
+    
+});
+
+app.get("/bacon",(req,res) =>{
+    res.render("bacon.ejs");
+    
+});
+
+app.get("/imgflip",(req,res) =>{
+    res.render("imgflip.ejs");
+    
+});
+
+app.get("/blackjack",(req,res)=>{
+    res.render("blackjack.ejs");
+})
+
+app.get("/nasa",(req,res) =>{
+    res.render("nasa.ejs");
+    
+});
+
+app.get("/random",(req,res)=>{
+    var page = pages[Math.floor(Math.random() * numPages)];
+    console.log(page);
+    res.redirect(page);
+})
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
