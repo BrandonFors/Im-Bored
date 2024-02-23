e3e09f273a41a3d09b382b5b971ae15bef3d9477
Brandon Forseth <bfclashman15@gmail.com>
2024-02-23 11:53:19 -0700

    Need arrays of hands to clean up

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const numPages = 4;
 //imgflip variables
 var letters = /[a-z]/i;
 //blackjack variables
+//MAYBE MAKE AN ARRAY OF HANDS
 const BLACKJACK_API_URL = "https://www.deckofcardsapi.com/api/deck";
 var deckId;
 var remainingCards;
 var currentBet;
 var numHands;
 var dealerCards = [];
+var hiddenCard = [];
 var playerCards = [];
+var totalValues = [];
+var bust = false;
+var message;
+var gameState;
+var buttons = [];
 
 
 
@@ -80,10 +87,31 @@ app.get("/imgflip",(req,res) =>{
 
 //////////////////////////////////////////////////////////////BLACKJACK\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 app.get("/blackjack",(req,res)=>{
+    
     res.render("blackjack.ejs");
 })
 app.get("/blackjack/play",async(req,res)=>{
-      res.render("blackjack.ejs", {dealer:dealerCards,player:playerCards});
+    
+    switch(gameState) {
+        case "inPlay":
+          message = "Select 'Hit' or 'Stand'. Don't get over 21!";
+          buttons = [
+            {ref:"/blackjack/play/hit", text: "HIT"},
+            {ref:"/blackjack/play/stand", text: "STAND"}
+        ];
+          break;
+        case "betting":
+            message = "Place and lock in your bet.";
+          break;
+        case "busted":
+            message = "You busted all over the place.";
+            break;
+          
+        default:
+          message = "";
+      }
+
+      res.render("blackjack.ejs", {dealer:dealerCards,player:playerCards,numHands:numHands,message:message});
 })
 app.get("/blackjack/play/shuffle", async (req,res)=>{
     try {
@@ -92,7 +120,7 @@ app.get("/blackjack/play/shuffle", async (req,res)=>{
         });
         console.log(response);
         deckId = response.data.deck_id;
-
+        gameState = "betting";
         res.redirect("/blackjack/play");
       } catch (error) {
         res.status(500).json({ message: "Error fetching data" });
@@ -100,18 +128,46 @@ app.get("/blackjack/play/shuffle", async (req,res)=>{
 })
 app.post("/blackjack/play/deal",async(req,res)=>{
     currentBet = req.body.bet;
+    numHands = req.body.hands;
     try {
         const response = await axios.get(`${BLACKJACK_API_URL}/${deckId}/draw/`,{
             params:{count:numHands*2+2}
         });
+        dealerCards.push[response.cards[0]];
+        hiddenCard.push[response.cards[numHands+1]];
+        for(x=1;x===numHands*2+2;x++){
+            if(!(x===numHands*2+2)){
+                playerCards.push(response.cards[x]);
+            }
+        for(x=0;x===numhands*2-1;x++){
+            totalValues[(x)%numHands] = totalValues[(x)%numHands] +parseInt(playerCards[x].value);
+        }    
+        }
+        gameState = "inPlay";
         console.log(response);
-        deckId = response.data.deck_id;
         res.redirect("/blackjack/play");
       } catch (error) {
         res.status(500).json({ message: "Error fetching data" });
       }
 
 });
+app.get("/blackjack/play/hit",async(req,res)=>{
+    try {
+        const response = await axios.get(`${BLACKJACK_API_URL}/${deckId}/draw/`,{
+            params:{count:1}
+        });
+        
+        console.log(response);
+        playerCards.push(response.cards[0]);
+        
+        playerCards.forEach((card)=>{parseInt(card.value)});
+        res.redirect("/blackjack/play");
+      } catch (error) {
+        res.status(500).json({ message: "Error fetching data" });
+      }
+
+});
+//CREATE STAND FUNCTION
 /////////////////////////////////////////////////////////NASA\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 app.get("/nasa",(req,res) =>{
     res.render("nasa.ejs");
