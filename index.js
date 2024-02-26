e7e61bfd10f95ca3b36093a7716698d2b7e5a015
Brandon Forseth <bfclashman15@gmail.com>
2024-02-26 14:21:24 -0700

    RESPONSE TIMES ARE GOOOOOFY

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -19,8 +20,6 @@ var numHands;
 var dealerCards = [];
 var hiddenCard = [];
 var playerCards = [];
-var totalValues = [];
-var bust = false;
 var message;
 var gameState;
 var buttons = [];
@@ -88,6 +87,11 @@ app.get("/imgflip",(req,res) =>{
 
 
 //////////////////////////////////////////////////////////////BLACKJACK\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+function calcTotalValue(hand){
+    var totalValue = 0;
+    hand.cards.forEach((card)=>{totalValue+=parseInt(card.value)})
+    return totalValue;
+}
 app.get("/blackjack",(req,res)=>{
     
     res.render("blackjack.ejs");
@@ -104,10 +108,15 @@ app.get("/blackjack/play",async(req,res)=>{
           break;
         case "betting":
             message = "Place and lock in your bet.";
+            buttons = [
+                {ref:"/blackjack/play/hit", text: "HIT"},
+                {ref:"/blackjack/play/stand", text: "STAND"}
+            ];
           break;
-        case "busted":
-            message = "You busted all over the place.";
-            break;
+        //not in use
+        // case "busted":
+        //     message = "You busted all over the place.";
+        //     break;
           
         default:
           message = "";
@@ -116,13 +125,16 @@ app.get("/blackjack/play",async(req,res)=>{
       res.render("blackjack.ejs", {dealer:dealerCards,player:playerHands,numHands:numHands,message:message});
 })
 app.get("/blackjack/play/shuffle", async (req,res)=>{
+    console.log('shuffle');
     try {
-        const response = await axios.get(`${BLACKJACK_API_URL}/new/shuffle/`,{
+        
+        const response = await axios.get(`https://www.deckofcardsapi.com/api/deck/new/shuffle`,{
             params:{deck_count:6}
         });
-        console.log(response);
+        console.log(response.data);
         deckId = response.data.deck_id;
         gameState = "betting";
+        console.log("deckID: " + deckId);
         res.redirect("/blackjack/play");
       } catch (error) {
         res.status(500).json({ message: "Error fetching data" });
@@ -130,23 +142,42 @@ app.get("/blackjack/play/shuffle", async (req,res)=>{
 })
 app.post("/blackjack/play/deal",async(req,res)=>{
     currentBet = req.body.bet;
-    numHands = req.body.hands;
+    numHands = parseInt(req.body.hands);
+    playerHands = [];
+    playerCards = [];
     try {
         const response = await axios.get(`${BLACKJACK_API_URL}/${deckId}/draw/`,{
             params:{count:numHands*2+2}
         });
-        dealerCards.push[response.cards[0]];
-        hiddenCard.push[response.cards[numHands+1]];
-        for(x=1;x===numHands*2+2;x++){
-            if(!(x===numHands*2+2)){
-                playerCards.push(response.cards[x]);
+        //gives dealer a card and has a hidden card
+        console.log(response.data);
+        dealerCards.push(response.data.cards[0]);
+        hiddenCard.push(response.data.cards[numHands+1]);
+        
+        for(var x=1;x<=numHands;x++){
+          playerHands.push({
+            id: x,
+            cards: [],
+            totalValues: 0,
+            bust: false,
+          });  
+        }
+        //puts appropriate player cards from response into array
+        for(var x=1;x<response.data.cards.length;x++){
+            if(!(x==numHands+1)){
+                playerCards.push(response.data.cards[x]);
             }
-        for(x=0;x===numhands*2-1;x++){
-            totalValues[(x)%numHands] = totalValues[(x)%numHands] +parseInt(playerCards[x].value);
-        }    
+        for(var x=0; x<playerCards.length;x++ ){
+            playerHands[x].cards.push(playerCards[(x%3)]);
+        }
+        for(var x=0; x<numHands;x++ ){
+            playerHands[x].totalValues=calcTotalValue(playerHands[x]);
+        }
+   
         }
         gameState = "inPlay";
-        console.log(response);
+        console.log(response.data);
+        console.log(playerHands);
         res.redirect("/blackjack/play");
       } catch (error) {
         res.status(500).json({ message: "Error fetching data" });
@@ -159,8 +190,8 @@ app.get("/blackjack/play/hit",async(req,res)=>{
             params:{count:1}
         });
         
-        console.log(response);
-        playerCards.push(response.cards[0]);
+        console.log(response.data);
+        playerCards.push(response.data.cards[0]);
         
         playerCards.forEach((card)=>{parseInt(card.value)});
         res.redirect("/blackjack/play");
